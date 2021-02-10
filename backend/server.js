f710ff691f496429c5b587134b40de07ff3a7936
ThiagoSFerraz <oniferraz@gmail.com>
2021-02-10 20:57:26 -0300

    server backend

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const rotas = require("./routes/consultas");
+
+var app = express();
+
+app.use((req, res, next) => {
+	
+    res.header("Access-Control-Allow-Origin", "*");
+	
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    
+    app.use(cors());
+    
+    next();
+});
+
+app.use(bodyParser.json());
+
+app.use("/", rotas);
+
+
+app.listen(4000);
