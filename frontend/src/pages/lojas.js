cd6f9a57a6d3a5be8099ec9e7af1d31924ce76b6
ThiagoSFerraz <oniferraz@gmail.com>
2021-02-11 15:24:06 -0300

    Lazy Loading Lojas

--- frontend/src/pages/lojas.js
+++ frontend/src/pages/lojas.js
@@ -1,23 +1,24 @@
-import {lazy, Suspense} from 'react'
-import '../components/lojas/lojas.css'
-const ListaLoja = lazy(() => import('../components/lojas'))
+import { lazy, Suspense } from "react";
+import "../components/lojas/style.css";
+import React from "react";
 
-
+const ListaLoja = lazy(() => import("../components/lojas"));
 
 export default function Lojas() {
-    return (
-    <div className="fundo">
-        <div className="container-fluid bg-dark">
-            <div className="row justify-content-center text-center  text-white">
-                <div className="col-12 mt-5">
-                    <h1>Essas são nossas localizações!</h1>
-                </div>
-            </div>
-            {/* Lazy Loading */}
-            <Suspense fallback={<h2 className="text-white text-center">Carregando...</h2>}>
-                <ListaLoja/>
-            </Suspense>
+  return (
+    <div className="bg-dark fundo">
+      <div className="container-fluid bg-dark">
+        <div className="row justify-content-center text-center  text-white">
+          <div className="col-12 mt-5">
+            <h1>Essas são nossas localizações!</h1>
+          </div>
         </div>
+        {/* Lazy Loading */}
+        <Suspense
+          fallback={<h2 className="text-white text-center">Carregando...</h2>}>
+          <ListaLoja />
+        </Suspense>
+      </div>
     </div>
-    )
+  );
 }
