8788e7ac36ff9f9dc6557fb782c30f9c072fdd86
ThiagoSFerraz <oniferraz@gmail.com>
2021-02-12 16:41:12 -0300

    server

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const rotas = require("./routes/consultas");
+const rotas = require("./src/routes/consultas");
 
 var app = express();
 
 app.use((req, res, next) => {
 	
     res.header("Access-Control-Allow-Origin", "*");
+    
+    res.header("Access-Control-Allow-Headers","*")
 	
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     
