aa58a51d662297ec5964f3698724b394b867b4af
ThiagoSFerraz <oniferraz@gmail.com>
2021-05-02 11:44:17 -0300

    pequenas adições

--- frontend/src/components/menu/index.js
+++ frontend/src/components/menu/index.js
@@ -1,37 +1,62 @@
-import { Navbar, Nav } from 'react-bootstrap';
-import { Link, withRouter } from 'react-router-dom';
-
-
+import { Navbar, Nav } from "react-bootstrap";
+import { Link, withRouter } from "react-router-dom";
 
 function BaseMenu(props) {
-    const { location } = props
-    return(
-        
-        <Navbar className="navbar" bg="danger" expand="lg">
-            <Navbar.Brand className=" text-white">
-                FullStackEletro &copy;
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="item-menu" />
-            <Navbar.Collapse id="item-menu" data-toggle="collapse">
-                <Nav className="ml-auto text-white mr-3 px-5">
-                    <Nav.Item>
-                        <Nav.Link className="px-4 text-white" as={Link} href="/" to="/">Página Inicial</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link className="px-4 text-white"  as={Link} href="/produtos" to="/produtos" >Produtos</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link className="px-4 text-white" as={Link} href="/pedidos" to="/pedidos" >Pedidos</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link className="px-4 text-white" as={Link} href="/contato" to="/contato" >Entre em contato</Nav.Link>
-                    </Nav.Item>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    )
+  const { location } = props;
+  return (
+    <Navbar className="navbar" bg="danger" expand="lg">
+      <Navbar.Brand className=" text-white">
+        FullStackEletro &copy;
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="item-menu" />
+      <Navbar.Collapse id="item-menu" data-toggle="collapse">
+        <Nav className="ml-auto text-white mr-3 ">
+          <Nav.Item>
+            <Nav.Link
+              className="px-md-4 px-sm-0 text-white"
+              as={Link}
+              href="/"
+              to="/"
+            >
+              Página Inicial
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              className="px-md-4 px-sm-0 text-white"
+              as={Link}
+              href="/produtos"
+              to="/produtos"
+            >
+              Produtos
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              className="px-md-4 px-sm-0 text-white"
+              as={Link}
+              href="/pedidos"
+              to="/pedidos"
+            >
+              Pedidos
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              className="px-md-4 px-sm-0 text-white"
+              as={Link}
+              href="/contato"
+              to="/contato"
+            >
+              Entre em contato
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
-const Menu = withRouter(BaseMenu)
+const Menu = withRouter(BaseMenu);
 
-export default Menu
+export default Menu;
